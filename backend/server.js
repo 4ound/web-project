27b053b87b09ca5498675e2d9be05fcce734980a
4ound <lnfedorov18@gmail.com>
2020-11-15 15:27:55 +0300

    adding openweather to server-side

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
-const express = require("express")
-const app = express()
+import express from "express";
+import { OpenWeather } from "./open_weather.js";
+// const openWeather = require("./open_weather")
+const app = express();
 
 app.get("/", function (req, res) {
     // res.send(process.env.RESPONSE)
-    res.send("Hello world!!!")
+    res.send(new URL("https://api.openweathermap.org/data/2.5/weather"));
 })
+
+app.get("/r", (req, res) => {
+    OpenWeather.getByName("Ижевск").then(r => {
+        res.send(r);
+    });
+})
+
 app.listen(process.env.PORT)
